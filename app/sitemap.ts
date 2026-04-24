b87493f9770d08d53807c989eb209e8be506513d
Nick Dame <nicholas.dame@gmail.com>
2026-04-24 09:56:46 -0700

    sitemap move

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
 
-const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://rvintel.vercel.app'
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://rvintel.io'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
